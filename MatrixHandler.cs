32f46b1a0f178abb3b9c9be1e3023a7b6aec1820
Vadim <vadimogienko42k@gmail.com>
2025-04-11 17:41:31 +0700

    Создан делегат и анонимный метод приведения матрицы к диагональному виду

---
+++ MatrixHandler.cs
@@ -0,0 +1,42 @@
+public abstract class MatrixHandler {
+  protected MatrixHandler next;
+  public void SetNext(MatrixHandler nextHandler) => next = nextHandler;
+  public abstract void Handle(SquareMatrix matrix);
+}
+
+public class TransposeHandler : MatrixHandler {
+  public override void Handle(SquareMatrix matrix) {
+    Console.WriteLine("\nТранспонированная матрица:");
+    Console.WriteLine(matrix.Transpose());
+    next?.Handle(matrix);
+  }
+}
+
+public class TraceHandler : MatrixHandler {
+  public override void Handle(SquareMatrix matrix) {
+    Console.WriteLine($"След матрицы: {matrix.Trace():F2}");
+    next?.Handle(matrix);
+  }
+}
+
+public class DiagonalHandler : MatrixHandler {
+  public override void Handle(SquareMatrix matrix) {
+    Action<SquareMatrix> toDiagonal = delegate (SquareMatrix m) {
+      for (int i = 0; i < m.Size; i++)
+        for (int j = 0; j < m.Size; j++)
+          if (i != j) m.Data[i, j] = 0;
+    };
+
+    toDiagonal(matrix);
+    Console.WriteLine("Матрица после приведения к диагональному виду:");
+    Console.WriteLine(matrix);
+
+    next?.Handle(matrix);
+  }
+}
+
+public class ExitHandler : MatrixHandler {
+  public override void Handle(SquareMatrix matrix) {
+    Console.WriteLine("Завершение цепочки обработки.\n");
+  }
+}
